1f24383809b814099cf4589ae76f3cd2ec325cfb
Delfina Laora <delphine2203.miel@gmail.com>
2023-06-08 11:37:16 +0200

    Ajout des filtres de catégorie

--- Backend/images/gallery.js
+++ Backend/images/gallery.js
@@ -1,7 +1,10 @@
+// const { categories } = require("../models");
 
+
+// const { works } = require("../models");
 
 const workGallery = await fetch("http://localhost:5678/api/works").then(works => works.json()) /*on récupère l'API puis on la transforme en json*/
-
+const categoryGallery = await fetch("http://localhost:5678/api/categories").then(categories => categories.json()) 
 // Appel des balises
 // Récupération de l'élément du DOM qui accueillera les porfolio
 const portfolio = document.querySelector("#portfolio");
@@ -31,8 +34,51 @@ function genererWorks(workGallery){
 
 genererWorks(workGallery);
 
+const buttonWorks = document.querySelector(".tous");
+
+buttonWorks.addEventListener("click", function(){
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(workGallery);  
+});
+
+const buttonObjet = document.querySelector(".objet");
+
+buttonObjet.addEventListener("click", function(){
+
+        const objetFiltrees = workGallery.filter(function(work){
+            return work.categoryId == 1;
+            });   
+    
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(objetFiltrees)    
+});
+
+const buttonAppart = document.querySelector(".appart");
+
+buttonAppart.addEventListener("click", function(){
+
+        const appartFiltrees = workGallery.filter(function(work){
+            return work.categoryId == 2;
+            });   
+    
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(appartFiltrees)    
+});
+
+const buttonHotel = document.querySelector(".hotel");
+
+buttonHotel.addEventListener("click", function(){
+
+        const hotelFiltrees = workGallery.filter(function(work){
+            return work.categoryId == 3;
+            });   
+    
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(hotelFiltrees)    
+});
 
 
-
-
+console.log(workGallery)
+console.log(categoryGallery)
+console.log(workGallery[0].categoryId)
 
